d75bdf4afc068305ce0cef5057203f5e18b66171
Philipp Weckerle <philipp.weckerle@microsoft.com>
2019-03-28 17:32:30 +0100

    streamline script names

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log ('*** Azure Discovery Day 2019 - Vienna');
 console.log ('*** 26.3.2019');
 console.log();
-console.log ('usage : npm run [ o | r | a ]');
-console.log ('options : [o]riginal, [r]efactored or re[a]rchitected')
+console.log ('usage : npm run [ h | f | a ]');
+console.log ('options : re[h]ost, re[f]actored or re[a]rchitected')
