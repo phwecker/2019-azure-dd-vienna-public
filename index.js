2b152ac797b893f4e12029c971c57836e05f49cc
Philipp Weckerle <philipp.weckerle@microsoft.com>
2019-03-28 15:01:13 +0100

    updated run scripts

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log ('*** Azure Discovery Day 2019 - Vienna');
+console.log ('*** 26.3.2019');
+console.log();
+console.log ('usage : npm run [ o | r | a ]');
+console.log ('options : [o]riginal, [r]efactored or re[a]rchitected')
