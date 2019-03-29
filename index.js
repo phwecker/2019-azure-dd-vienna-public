6e56d481d84e22cc28669009d2ac88ff6903d341
Philipp Weckerle <philipp.weckerle@microsoft.com>
2019-03-29 07:48:57 +0100

    naming convention update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log ('*** Azure Discovery Day 2019 - Vienna');
 console.log ('*** 26.3.2019');
 console.log();
-console.log ('usage : npm run [ h | f | a ]');
-console.log ('options : re[h]ost, re[f]actored or re[a]rchitected')
+console.log ('usage : npm run [ h | p | a ]');
+console.log ('options : re[h]ost, re[p]latform or re[a]rchitected')
